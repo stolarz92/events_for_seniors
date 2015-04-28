c0b49d568c727e41c8447c603f9ed35147778623
Radosław Stolarski <stolarz92@gmail.com>
2015-04-29 00:04:08 +0200

    Create homepage controller, basic layout

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 #bootstrap
-gem 'bootstrap-sass', '3.2.0.0'
+gem 'bootstrap-sass', '3.3.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  get '/' => 'homepage#index'
+
   resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'events#index'
+  root 'homepage#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
