146ec944c9eac7fa2438b18a4b2d2ca22d570bc1
Radosław Stolarski <stolarz92@gmail.com>
2015-06-17 12:01:42 +0200

    Add image to city model, update city seeds

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,5 @@
 class CitiesController < ApplicationController
-  before_action :set_city
+  before_action :set_city, only: [:index]
 
   def index
     if params[:search] && params[:date] && @city
@@ -9,13 +9,26 @@ class CitiesController < ApplicationController
   end
 
   def show
-
+    @city = City.find(params[:id])
   end
 
   def new
+    @city = City.new
   end
 
+
   def create
+    @city = City.new(city_params)
+
+    respond_to do |format|
+      if @city.save
+        format.html { redirect_to @city, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @city }
+      else
+        format.html { render :new }
+        format.json { render json: @city.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -27,4 +40,11 @@ class CitiesController < ApplicationController
   def destroy
   end
 
+  private
+  def city_params
+    params.require(:city).permit(
+        :name,
+        :image,
+    )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610113708) do
+ActiveRecord::Schema.define(version: 20150617090400) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -20,9 +20,13 @@ ActiveRecord::Schema.define(version: 20150610113708) do
   end
 
   create_table "cities", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",               limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "events", force: :cascade do |t|
