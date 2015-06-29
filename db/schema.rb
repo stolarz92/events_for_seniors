bf2dd2b390d27b4f874768776f42639bc27dea79
Radosław Stolarski <stolarz92@gmail.com>
2015-06-29 09:43:26 +0200

    Add description to cities, change seeds and city view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626165021) do
+ActiveRecord::Schema.define(version: 20150629070554) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -21,12 +21,13 @@ ActiveRecord::Schema.define(version: 20150626165021) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",               limit: 255
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "image_file_name",    limit: 255
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+    t.text     "description",        limit: 65535
   end
 
   create_table "events", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,11 +31,52 @@ def create_user
   )
 end
 def create_cities
-  City.create([{ name: "Kraków", image: File.new("/home/epi/12_stolarski/obrazy/miasta/krakow.jpg")},
-               {name: "Warszawa", image: File.new("/home/epi/12_stolarski/obrazy/miasta/warszawa.jpg")},
-               {name: "Wrocław", image: File.new("/home/epi/12_stolarski/obrazy/miasta/wroclaw.jpg")},
-               {name: "Poznań", image: File.new("/home/epi/12_stolarski/obrazy/miasta/poznan.jpg")},
-               {name: "Gdańsk", image: File.new("/home/epi/12_stolarski/obrazy/miasta/gdansk.jpg")}])
+  City.create([{
+                   name: "Kraków",
+                   description: "<p>Kraków należy do grona najpiękniejszych miast Europy.
+Liczne walory turystyczne m. in. zabytki Krakowa oraz bogata oferta kulturalna miasta zostały już docenione na całym świecie.
+W 2005 r. Kraków zajął 5 miejsce na liście najciekawszych miast Europy, przygotowanej przez prestiżowy magazyn „Travel and Leisure”.
+Natomiast w 2006 r. amerykańska agencja Orbitz uznała gród Kraka za najmodniejsze miejsce na świecie.</p>
+
+<h3>Co zobaczyć w Krakowie.</h3>
+
+<p>Kraków jest centrum administracyjnym Małopolski oraz jednym z najważniejszych ośrodków gospodarczych, naukowych i kulturalnych kraju.
+ do 1795 r. Kraków był oficjalną stolicą Polski, a przypominają o tym liczne w mieście zabytki.</p>
+<p>Większość z nich usytuowana jest wokół słynnej Drogi Królewskiej, która rozpoczyna się na Kleparzu, wiedzie obok pozostałości średniowiecznych fortyfikacji –
+Bramy Floriańskiej, Barbakanu, baszt obronnych – dalej biegnie ulicą Floriańską aż na Rynek Główny, gdzie mija kościół Mariacki i Sukiennice,
+a następnie ulicą Grodzką dochodzi do Wzgórza Wawelskiego, gdzie usytuowany jest Zamek Królewski i Katedra Wawelska. Krakowska Via Regia przecina Planty –
+najbardziej reprezentacyjny park miasta, a wokół niej zlokalizowane jest także słynne Muzeum Książąt Czartoryskich,
+Teatr Stary im. Heleny Modrzejewskiej oraz zabytkowe budynki Uniwersytetu Jagiellońskiego, w tym najstarsze Collegium Maius.
+W Krakowie istniała również przez wieki znacząca gmina żydowska.</p>
+<p>Obszar zamieszkiwany przez Żydów – Krakowski
+Kazimierz – jest dziś miejscem, gdzie tradycja wspaniale współgra z nowoczesnością, gdzie w sąsiedztwie pięknych synagog funkcjonują popularne kawiarnie i kluby.
+ Na obszarze Kazimierza zlokalizowany jest też jeden z najznamienitszych panteonów narodowych – Krypta Zasłużonych na Skałce oraz słynna Fabryka Schindlera,
+której dzieje opowiedział w 1993 r. Steven Spielberg w filmie „Lista Schindlera”.</p>
+<p> W ostatnich latach Kraków stał się miejscem licznych pielgrzymek.
+Katolicy odwiedzają przede wszystkim Sanktuarium Bożego Miłosierdzia w Łagiewnikach i podchodzą pod „okno papieskie” Pałacu Biskupiego – miejsce, w którym mieszkał
+niegdyś Karol Wojtyła i skąd, już jako papież, prowadził on wielogodzinne rozmowy z wiernymi.</p>",
+                   image: File.new("/home/epi/12_stolarski/obrazy/miasta/krakow.jpg")
+               },
+               {
+                   name: "Warszawa",
+                   description: "",
+                   image: File.new("/home/epi/12_stolarski/obrazy/miasta/warszawa.jpg")
+               },
+               {
+                   name: "Wrocław",
+                   description: "",
+                   image: File.new("/home/epi/12_stolarski/obrazy/miasta/wroclaw.jpg")
+               },
+               {
+                   name: "Poznań",
+                   description: "",
+                   image: File.new("/home/epi/12_stolarski/obrazy/miasta/poznan.jpg")
+               },
+               {
+                   name: "Gdańsk",
+                   description: "",
+                   image: File.new("/home/epi/12_stolarski/obrazy/miasta/gdansk.jpg")
+               }])
 end
 
 def create_categories
@@ -67,18 +108,18 @@ def create_events
     effort = [1,2,3].sample
     image =
         ["/home/epi/12_stolarski/obrazy/DP-500S-01.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-02.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-03.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-04.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-05.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-06.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-07.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-08.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-09.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-10.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-11.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-12.jpg",
-        "/home/epi/12_stolarski/obrazy/DP-500S-13.jpg"].sample
+         "/home/epi/12_stolarski/obrazy/DP-500S-02.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-03.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-04.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-05.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-06.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-07.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-08.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-09.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-10.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-11.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-12.jpg",
+         "/home/epi/12_stolarski/obrazy/DP-500S-13.jpg"].sample
     user_id = [1,2,3,4,5].sample
     Event.create!(name: name,
                   description: description,
