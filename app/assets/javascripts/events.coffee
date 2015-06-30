d941017e91bb73f8cc21fd2f4d764bb9eb2b9fc8
Radosław Stolarski <stolarz92@gmail.com>
2015-06-30 10:05:13 +0200

    Descriptions of cities in seeds, ajax button to follow events

--- app/assets/javascripts/events.coffee
+++ app/assets/javascripts/events.coffee
@@ -1,2 +1,19 @@
 $ ->
-#  $(alert('alert'));
+  $('#button').click ->
+    event_id = $('#event_id').val()
+    if !event_id.trim()
+      return
+    $.post('/relationships',
+      event:
+        event_id: event_id,
+    ).success (resp) ->
+      console.log(resp)
+
+#      div_comment = $('<div>', { class: 'comment'})
+#      comment_text = "<h4>#{resp.comment.content}</h4> <p>#{resp.user_mail}</p> <p>#{resp.comment.created_at}</p>  "
+#      div_comment.html(comment_text)
+#      comments = $('#comments')
+#      $(div_comment).appendTo(comments)
+#      $('#comment_content').val('')
+#    .error (resp) ->
+#      alert('error')
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,21 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
   def create
-    event = Event.find(params[:event_id])
-    current_user.follow(event)
+    @user = current_user
+    @event = Event.find(params[:event_id])
+    current_user.follow(@event)
     respond_to do |format|
-      format.html { redirect_to @event }
+      format.html { redirect_to @user }
       format.js
     end
   end
 
   def destroy
-    event = Relationship.find(params[:id]).event
-    current_user.unfollow(event)
+    @user = current_user
+    @event = Relationship.find(params[:id]).event
+    @user.unfollow(@event)
     respond_to do |format|
-      format.html { redirect_to @event }
+      format.html { redirect_to @user }
       format.js
     end
   end
