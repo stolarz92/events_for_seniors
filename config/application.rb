8e4a0605d67845759073f509484e087d52ca622b
Radosław Stolarski <stolarz92@gmail.com>
2015-07-03 17:42:57 +0200

    Add ckeeditor

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,5 @@ module EventsForSeniors
 
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
-
-    config.assets.precompile += Ckeditor.assets
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   resources :events
   resources :relationships,       only: [:create, :destroy]
+  mount Ckeditor::Engine => '/ckeditor'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
